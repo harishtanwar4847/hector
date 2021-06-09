6c922bd3422d19f36f10976efb789f19bec3b790
anurag.mahadik <anurag.mahadik@atriina.com>
2021-06-09 16:06:44 +0530

    Deleted non-required doctypes from Hector App

--- hector/hector/doctype/customer/customer.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Atrina Technologies Pvt Ltd and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Customer', {
-	// refresh: function(frm) {
-
-	// }
-});
