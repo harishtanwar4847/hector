88a6262ad06a921fe1a59c5c79eb96d92feffb6d
anurag.mahadik <anurag.mahadik@atriina.com>
2021-06-22 11:11:56 +0530

    Deleted non-required doctypes

--- hector/patches/backup_patches/invoice_for_pode.py
+++
@@ -1,5 +0,0 @@
-import frappe
-from frappe.core.doctype.data_import.data_import import import_file
-def execute():
-    path = frappe.get_app_path('hector','patches','imports','invoice_for_pode.csv')
-    import_file('Workflow', path, 'Insert')
