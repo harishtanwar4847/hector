b88746546327c3c9383dca6399b2d7f9ba4eb1f6
anurag.mahadik <anurag.mahadik@atriina.com>
2021-06-25 16:38:10 +0530

    Role based permission_query_condition

--- hector/permissions.py
+++ hector/permissions.py
@@ -1,7 +1,9 @@
 import frappe
 
-def customer_query(user):
-    if not user:
-        user = frappe.session.user
+def customer_query(user = frappe.session.user):
+    if('Regional Sales Manager' in frappe.get_roles(user)):
         # return "(`tabCustomer`.rsm = '{user}')".format(user=frappe.session.user)
         return "(`tabCustomer`.owner in (SELECT `tabCustomer`.owner FROM `tabCustomer` INNER JOIN `tabSales Hierarchy Mapping` ON `tabCustomer`.owner = `tabSales Hierarchy Mapping`.so_user OR `tabCustomer`.owner = `tabSales Hierarchy Mapping`.ase_user OR `tabCustomer`.owner = `tabSales Hierarchy Mapping`.asm_user OR `tabCustomer`.owner = `tabSales Hierarchy Mapping`.rsm_user where `tabSales Hierarchy Mapping`.so_user = '{user}' OR `tabSales Hierarchy Mapping`.ase_user = '{user}' OR `tabSales Hierarchy Mapping`.asm_user = '{user}' OR `tabSales Hierarchy Mapping`.rsm_user = '{user}'))".format(user=frappe.session.user)
+
+    if('Customer Master Approver' in frappe.get_roles(user)):
+        return "(`tabCustomer`.naming_series = 'CUST-.YYYY.-')"
