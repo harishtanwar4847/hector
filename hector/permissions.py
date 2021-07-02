d9e90b3d8adf46de32c3279966c44712b6594e8b
anurag88 <anuragmahadik8@gmail.com>
2021-07-02 16:45:17 +0530

    Added new doctype Customer Onboarding Document

--- hector/permissions.py
+++ hector/permissions.py
@@ -9,3 +9,13 @@ def customer_query(user = frappe.session.user):
     if('Customer Master Approver' in frappe.get_roles(user) or 'Sales Manager' in frappe.get_roles(user) or 'Sales User' in frappe.get_roles(user) or 'System Manager' in frappe.get_roles(user) or 'Sales Master Manager' in frappe.get_roles(user)):
         print('\n\n\n<<<<<<<<<<<<Inside CMA MAPPING QUERY>>>>>>>>>>>>\n\n\n')
         return "(`tabCustomer`.naming_series = 'CUST-.YYYY.-')"
+
+def customer_onboarding_document_query(user = frappe.session.user):
+    if('Regional Sales Manager' in frappe.get_roles(user) or 'Area Sales Manager' in frappe.get_roles(user)):
+        print('\n\n\n<<<<<<<<<<<<Inside RSM MAPPING QUERY>>>>>>>>>>>>\n\n\n')
+        # return "(`tabCustomer Onboarding Document`.rsm = '{user}')".format(user=frappe.session.user)
+        return "(`tabCustomer Onboarding Document`.owner in (SELECT `tabCustomer Onboarding Document`.owner FROM `tabCustomer Onboarding Document` INNER JOIN `tabSales Hierarchy Mapping` ON `tabCustomer Onboarding Document`.owner = `tabSales Hierarchy Mapping`.so_user OR `tabCustomer Onboarding Document`.owner = `tabSales Hierarchy Mapping`.ase_user OR `tabCustomer Onboarding Document`.owner = `tabSales Hierarchy Mapping`.asm_user OR `tabCustomer Onboarding Document`.owner = `tabSales Hierarchy Mapping`.rsm_user where `tabSales Hierarchy Mapping`.so_user = '{user}' OR `tabSales Hierarchy Mapping`.ase_user = '{user}' OR `tabSales Hierarchy Mapping`.asm_user = '{user}' OR `tabSales Hierarchy Mapping`.rsm_user = '{user}'))".format(user=frappe.session.user)
+
+    if('Customer Onboarding Document Master Approver' in frappe.get_roles(user) or 'Sales Manager' in frappe.get_roles(user) or 'Sales User' in frappe.get_roles(user) or 'System Manager' in frappe.get_roles(user) or 'Sales Master Manager' in frappe.get_roles(user)):
+        print('\n\n\n<<<<<<<<<<<<Inside CMA MAPPING QUERY>>>>>>>>>>>>\n\n\n')
+        return "(`tabCustomer Onboarding Document`.naming_series = 'CUST-.YYYY.-')"
