acb9dc0770435415a72cc4d529c9ac573f66c32d
Neel Bhanushali <neel@atriina.com>
2021-08-17 06:42:59 +0000

    Update hector/__init__.py, CHANGELOG.md files

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [Unreleased]
+
+### Added
+- [Approval notifications](https://gitlab.com/atri-tech/atri-maintainers/hector/-/merge_requests/7)
--- hector/__init__.py
+++ hector/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import frappe
 import frappe.desk.desktop
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 # def get_page_for_user(self):
 #   roles_list = frappe.get_all("Has Role",filters={'parent':frappe.session.user},fields=['role'])
