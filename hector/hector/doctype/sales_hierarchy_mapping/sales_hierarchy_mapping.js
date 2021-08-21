54a1422e8a3bd86e9e91e64459722da0674639fb
Atri <om.satam@atriina.com>
2021-08-21 12:25:53 +0530

    added client scripts for customer form and SHM

--- hector/hector/doctype/sales_hierarchy_mapping/sales_hierarchy_mapping.js
+++ hector/hector/doctype/sales_hierarchy_mapping/sales_hierarchy_mapping.js
@@ -1,8 +1,11 @@
-// Copyright (c) 2021, Atrina Technologies Pvt Ltd and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Sales Hierarchy Mapping', {
-	// refresh: function(frm) {
-
-	// }
-});
+	refresh(frm) {
+		// your code here
+	},
+	before_save(frm){
+	    frm.set_value('so_name', (frm.doc.so_user) ? frm.doc.so_name : '' );
+	    frm.set_value('ase_name', (frm.doc.ase_user) ? frm.doc.ase_name : '' );
+	    frm.set_value('asm_name', (frm.doc.asm_user) ? frm.doc.asm_name : '' );
+	    frm.set_value('rsm_name', (frm.doc.rsm_user) ? frm.doc.rsm_name : '' );
+	}
+})
