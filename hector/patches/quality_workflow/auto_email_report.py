37ddc783140d4c5835b8cd6306f8f6a3a1b0e739
OM Satam <om.satam@atriina.com>
2021-09-20 11:36:40 +0000

    Update hector/patches/quality_workflow/auto_email_report.py

--- hector/patches/quality_workflow/auto_email_report.py
+++ hector/patches/quality_workflow/auto_email_report.py
@@ -2,5 +2,4 @@ import frappe
 from frappe.core.doctype.data_import.data_import import import_file
 def execute():
     path = frappe.get_app_path('hector','patches','quality_workflow','imports','auto_email_report.csv')
-    # file_path = '"'+path+
     import_file('Auto Email Report', path, 'Insert')
