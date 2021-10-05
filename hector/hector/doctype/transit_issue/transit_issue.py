237b68a3ff53ca4adec4234ee5346e8822e7baad
Atri <om.satam@atriina.com>
2021-10-05 18:39:17 +0530

    modified approval email notifications according to email templates

--- hector/hector/doctype/transit_issue/transit_issue.py
+++ hector/hector/doctype/transit_issue/transit_issue.py
@@ -19,104 +19,210 @@ class TransitIssue(Document):
 		financeTeamName = [x for x in frappe.db.sql("""select u.full_name from `tabUser` u inner join `tabHas Role` hr on hr.parent = u.name where hr.role = 'Finance Team'""", as_list=1)]
 		financeTeamName.remove(['Administrator'])
 		financeTeamName = financeTeamName[0][0]
+		emailMessage = """You have been requested to review the following:<br>
+			<table border="1" cellspacing="0" cellpadding="5" align="">
+				<tbody>
+					<tr>
+						<td>Customer Name:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Code:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Location:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Phone Number:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Type of Issue:</td>
+						<td>{}</td>
+					</tr>
+				</tbody>
+			</table><br>
+			Kindly login to apps.myhector.com for the approval process.<br><br><br>
+			Regards,<br>
+			Hector Beverages""".format(self.customer_name, self.customer_code, self.customer_location, self.customer_phone_number, self.type_of_issue)
 
 
 		if self.workflow_state == 'Pending for Supply Team Approval' :
 			#For sendng email to sales team of customer complaint registered
-			msg="""Hello {},<br><br>
-				Your Complaint has been registered.<br><br>
-				Kindly login to apps.myhector.com for the approval process.<br><br><br>
-				Regards,<br>
-				Hector Beverages""".format(self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Complaint Registered", content=msg,recipients = '{}'.format(requestorSalesTeam), cc = '{},{}'.format(rsmEmail, asmEmail),expose_recipients="header", sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue registered: {}: {}".format(self.customer_code, self.customer_name), content=msg,recipients = '{}'.format(requestorSalesTeam), cc = '{},{}'.format(rsmEmail, asmEmail),expose_recipients="header", sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 			#For sending approval email to Supply Team
-			msg="""Hello Team,<br><br>
-			You have received a request for transit complaint from {} for the customer {}.<br><br>
-			Kindly login to apps.myhector.com for the approval process.<br><br><br>
-			Regards,<br>
-			Hector Beverages""".format(complaintTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Pending for Supply Team Approval", content=msg, recipients = supplyTeamEmail,sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue Pending for Supply Team Approval: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = supplyTeamEmail,sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
 		if self.workflow_state == 'Resent for Supply Team Approval' :
-			msg="""Hello Team,<br><br>
-			You have received a request for transit complaint from {} for the customer {}.<br><br>
-			Kindly login to apps.myhector.com for the approval process.<br><br><br>
-			Regards,<br>
-			Hector Beverages""".format(complaintTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Resent for Supply Team Approval", content=msg, recipients = supplyTeamEmail,sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue Resent for Supply Team Approval: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = supplyTeamEmail,sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
 		if self.workflow_state == 'Rejected by Supply Team' :
-			msg="""Hello {},<br><br>
-			You have received a request is rejected by Supply Team for the customer {}.<br><br>
+			msg="""Below Transit Issue have been Rejected:<br>
+			<table border="1" cellspacing="0" cellpadding="5" align="">
+				<tbody>
+					<tr>
+						<td>Customer Name:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Code:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Location:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Phone Number:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Type of Issue:</td>
+						<td>{}</td>
+					</tr>
+				</tbody>
+			</table><br>
+			Comment: {}<br><br>
 			Kindly login to apps.myhector.com for the approval process.<br><br><br>
 			Regards,<br>
-			Hector Beverages""".format(complaintTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Rejected by Supply Team", content=msg, recipients = '{}'.format(complaintTeamEmail),sender="Notification@hectorbeverages.com")
+			Hector Beverages""".format(self.customer_name, self.customer_code, self.customer_location, self.customer_phone_number, self.type_of_issue, self.reason_of_rejection)
+			frappe.sendmail(subject="Transit Issue Rejected: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = '{}'.format(complaintTeamEmail),sender="Notification@hectorbeverages.com")
+			print("\n email sent \n")
+			#For sendng email to sales team and customer of Issue rejected process
+			msg="""Below Transit Issue have been Rejected:<br>
+			<table border="1" cellspacing="0" cellpadding="5" align="">
+				<tbody>
+					<tr>
+						<td>Customer Name:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Code:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Location:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Phone Number:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Type of Issue:</td>
+						<td>{}</td>
+					</tr>
+				</tbody>
+			</table><br>
+			Comment: {}<br><br>
+			Kindly login to apps.myhector.com for the approval process.<br><br><br>
+			Regards,<br>
+			Hector Beverages""".format(self.customer_name, self.customer_code, self.customer_location, self.customer_phone_number, self.type_of_issue, self.reason_of_rejection)
+			frappe.sendmail(subject="Transit Issue Rejected: {}: {}".format(self.customer_code, self.customer_name), content=msg, cc = '{},{}'.format(rsmEmail, asmEmail), recipients = '{}'.format(requestorSalesTeam), expose_recipients="header", sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
+
 		if self.workflow_state == 'Requested for More Details by Supply Team':
-			msg="""Hello {},<br><br>
-			You have received a request for more details from Supply Team for the customer {}.<br><br>
-			Kindly login to apps.myhector.com for the approval process.<br><br><br>
-			Regards,<br>
-			Hector Beverages""".format(complaintTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Requested for More Details by Supply Team", content=msg, recipients = '{}'.format(complaintTeamEmail),sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue requesting for more details: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = '{}'.format(complaintTeamEmail),sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
 		if self.workflow_state == 'Requested for More Details by Finance Team':
-			msg="""Hello {},<br><br>
-			You have received a request for more details from {} for the customer {}.<br><br>
-			Kindly login to apps.myhector.com for the approval process.<br><br><br>
-			Regards,<br>
-			Hector Beverages""".format(complaintTeamName,financeTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Requested for More Details by Finance Team", content=msg, recipients = supplyTeamEmail,sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue requesting for more details: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = supplyTeamEmail,sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
 		if self.workflow_state == 'Rejected by Finance Team':
-			msg="""Hello Team,<br><br>
-			Your request for quality team approval is rejected by {} for the customer {}.<br><br>
+			msg="""Below Transit Issue have been Rejected:<br>
+			<table border="1" cellspacing="0" cellpadding="5" align="">
+				<tbody>
+					<tr>
+						<td>Customer Name:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Code:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Location:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Phone Number:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Type of Issue:</td>
+						<td>{}</td>
+					</tr>
+				</tbody>
+			</table><br>
+			Comment: {}<br><br>
 			Kindly login to apps.myhector.com for the approval process.<br><br><br>
 			Regards,<br>
-			Hector Beverages""".format(financeTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Rejected by Finance Team", content=msg, recipients = [complaintTeamEmail] + supplyTeamEmail, sender="Notification@hectorbeverages.com")
+			Hector Beverages""".format(self.customer_name, self.customer_code, self.customer_location, self.customer_phone_number, self.type_of_issue, self.reason_of_rejection)
+			frappe.sendmail(subject="Transit Issue Rejected: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = [complaintTeamEmail] + supplyTeamEmail, sender="Notification@hectorbeverages.com")
+			print("\n email sent \n")
+			#For sendng email to sales team and customer of Issue rejected process
+			msg="""Below Transit Issue have been Rejected:<br>
+			<table border="1" cellspacing="0" cellpadding="5" align="">
+				<tbody>
+					<tr>
+						<td>Customer Name:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Code:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Location:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Customer Phone Number:</td>
+						<td>{}</td>
+					</tr>
+					<tr>
+						<td>Type of Issue:</td>
+						<td>{}</td>
+					</tr>
+				</tbody>
+			</table><br>
+			Comment: {}<br><br>
+			Kindly login to apps.myhector.com for the approval process.<br><br><br>
+			Regards,<br>
+			Hector Beverages""".format(self.customer_name, self.customer_code, self.customer_location, self.customer_phone_number, self.type_of_issue, self.reason_of_rejection)
+			frappe.sendmail(subject="Transit Issue Rejected: {}: {}".format(self.customer_code, self.customer_name), content=msg, cc = '{},{}'.format(rsmEmail, asmEmail), recipients = '{}'.format(requestorSalesTeam), expose_recipients="header", sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
+
 		if self.workflow_state == 'Pending for Finance Team Approval':
-			msg="""Hello {},<br><br>
-			You have received a request for finance team approval from Supply Team for the customer {}.<br><br>
-			Kindly login to apps.myhector.com for the approval process.<br><br><br>
-			Regards,<br>
-			Hector Beverages""".format(financeTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Pending for Finance Team Approval", content=msg, recipients = '{}'.format(financeTeamEmail),sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue Pending for Finance Team Approval: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = '{}'.format(financeTeamEmail),sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
 		if self.workflow_state == 'Resent for Finance Team Approval':
-			msg="""Hello {},<br><br>
-			You have received a request for finance team approval from Supply Team for the customer {}.<br><br>
-			Kindly login to apps.myhector.com for the approval process.<br><br><br>
-			Regards,<br>
-			Hector Beverages""".format(financeTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Resent for Finance Team Approval", content=msg, recipients = '{}'.format(financeTeamEmail),sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue Resent for Finance Team Approval: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = '{}'.format(financeTeamEmail),sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
 		if self.workflow_state == 'Issue Closed':
-			msg="""Hello Team,<br><br>
-			Your request for Transit Complaint for the customer {} is Closed.<br><br>
-			Kindly login to apps.myhector.com for the approval process.<br><br><br>
-			Regards,<br>
-			Hector Beverages""".format(financeTeamName, self.customer_name)
-			frappe.sendmail(subject="Transit Complaints: Issue Closed", content=msg, recipients = [complaintTeamEmail] + supplyTeamEmail, sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue Completed: {}: {}".format(self.customer_code, self.customer_name), content=msg, recipients = [complaintTeamEmail] + supplyTeamEmail, sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
 			#For sendng email to sales team of credit note raised process
-			msg="""Hello Team,<br><br>
-				Credit Note raised for the customer {}.<br><br>
-				Kindly login to apps.myhector.com for the approval process.<br><br><br>
-				Regards,<br>
-				Hector Beverages""".format(self.customer_name)
-			frappe.sendmail(subject="Customer Complaints: Credit Note Raised", content=msg, cc = '{},{}'.format(rsmEmail, asmEmail), recipients = '{}'.format(requestorSalesTeam), expose_recipients="header", sender="Notification@hectorbeverages.com")
+			msg=emailMessage
+			frappe.sendmail(subject="Transit Issue Completed: {}: {}".format(self.customer_code, self.customer_name), content=msg, cc = '{},{}'.format(rsmEmail, asmEmail), recipients = '{}'.format(requestorSalesTeam), expose_recipients="header", sender="Notification@hectorbeverages.com")
 			print("\n email sent \n")
 
