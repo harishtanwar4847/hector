96a15b48b4564df7f35ad3c73b5674b80255c063
Atri <om.satam@atriina.com>
2021-10-11 19:06:50 +0530

    changed patch for workflow state and workflow action master to insert from update

--- hector/patches/quality_workflow/workflow_update/quality_workflow.py
+++ hector/patches/quality_workflow/workflow_update/quality_workflow.py
@@ -3,8 +3,8 @@ from frappe.core.doctype.data_import.data_import import import_file
 
 def execute():
     path = frappe.get_app_path('hector','patches','quality_workflow','imports','workflow_state.csv')
-    import_file('Workflow State', path, 'Update',console=True)
+    import_file('Workflow State', path, 'Insert',console=True)
     path = frappe.get_app_path('hector','patches','quality_workflow','imports','workflow_action_master.csv')
-    import_file('Workflow Action Master', path, 'Update',console=True)
+    import_file('Workflow Action Master', path, 'Insert',console=True)
     path = frappe.get_app_path('hector','patches','quality_workflow','imports','quality_workflow.csv')
     import_file('Workflow', path, 'Update',console=True)
