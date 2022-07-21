55168d29513f89e10b33bc32ab9a2a7103c0e55f
Harish Tanwar <harish.tanwar@atriina.com>
2022-07-21 14:09:19 +0530

    lead generation changes

--- hector/public/js/lead.js
+++ hector/public/js/lead.js
@@ -10,6 +10,8 @@ frappe.ui.form.on('Lead', {
     var gst_pattern = "^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$";
     var phone = frm.doc.phone || ""
     var pin = frm.doc.pincode || ""
+    var experience = frm.doc.enter_distribution_experience_in_years || ""
+    var number_list = ['0','1','2','3','4','5','6','7','8','9']
 
     if(phone.length > 0  && !phone.match(mobile_pattern))
         {
@@ -18,6 +20,28 @@ frappe.ui.form.on('Lead', {
     if(pin.length>0 && !pin.match(pin_pattern))
         {
             frappe.throw('Enter Valid Pin Code')
+        }
+    if (experience.length == 1){
+      if(!(number_list.includes(experience[0])))
+	        {
+	            frappe.throw('Enter Valid Experience in years')
+	        }   
+    }
+    if (experience.length == 2){
+      if(!(number_list.includes(experience[0]) && number_list.includes(experience[1])))
+	        {
+	            frappe.throw('Enter Valid Experience in years')
+	        }   
+    }
+    if (experience.length == 3){
+      if(!(number_list.includes(experience[0]) && number_list.includes(experience[1]) && number_list.includes(experience[2])))
+	        {
+	            frappe.throw('Enter Valid Experience in years')
+	        }   
+    }
+    if (experience.length>3)
+        {
+            frappe.throw('Experience in years can be a maximum 3 digit number')
         }
     
   }
