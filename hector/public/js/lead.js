19af214b884614146861a1343d2e7c8166665b9e
Harish Tanwar <harish.tanwar@atriina.com>
2022-08-09 15:33:12 +0530

    Field level changes and validations

--- hector/public/js/lead.js
+++ hector/public/js/lead.js
@@ -1,9 +1,12 @@
 frappe.ui.form.on('Lead', {
-	refresh: function(frm) {
+
+  refresh: function(frm) {
     if ((frm.doc.workflow_state == "Pending with Customer Support Team" && !frm.doc.customer_type && !frm.is_new()) || (frm.doc.workflow_state == "Pending with Customer Support Team" && !frm.doc.asm_user && !frm.is_new())){
       frm.set_intro("Please Enter Type of Customer, ASM and RSM")
     }
   },
+
+  
   validate(frm){
     var mobile_pattern = "^\\d{10,11}$";
     var pin_pattern = "^[1-9][0-9]{5}$"
@@ -11,11 +14,13 @@ frappe.ui.form.on('Lead', {
     var phone = frm.doc.phone || ""
     var pin = frm.doc.pincode || ""
     var experience = frm.doc.enter_distribution_experience_in_years || ""
+    var average_salary =  frm.doc.monthly_turn_over || ""
+    var vehicles = frm.doc.number_of_vehicles_for_distribution || ""
     var number_list = ['0','1','2','3','4','5','6','7','8','9']
 
     if(phone.length > 0  && !phone.match(mobile_pattern))
         {
-            frappe.throw('Enter Valid Mobile Number')
+            frappe.throw('Enter Valid Phone Number')
         }
     if(pin.length>0 && !pin.match(pin_pattern))
         {
@@ -24,25 +29,33 @@ frappe.ui.form.on('Lead', {
     if (experience.length == 1){
       if(!(number_list.includes(experience[0])))
 	        {
-	            frappe.throw('Enter Valid Experience in years')
+	            frappe.throw('Enter Valid distribution experience in years')
 	        }   
     }
     if (experience.length == 2){
       if(!(number_list.includes(experience[0]) && number_list.includes(experience[1])))
 	        {
-	            frappe.throw('Enter Valid Experience in years')
+	            frappe.throw('Enter Valid distribution experience in years')
 	        }   
     }
     if (experience.length == 3){
       if(!(number_list.includes(experience[0]) && number_list.includes(experience[1]) && number_list.includes(experience[2])))
 	        {
-	            frappe.throw('Enter Valid Experience in years')
+	            frappe.throw('Enter Valid distribution experience in years')
 	        }   
     }
     if (experience.length>3)
         {
-            frappe.throw('Experience in years can be a maximum 3 digit number')
+            frappe.throw('Distribution experience in years can be a maximum 3 digit number')
         }
+
+    if(average_salary > 9999){
+        frappe.throw('Average monthly sales in lakhs can be a maximum 4 digit number')
+    }
+    if(vehicles > 999){
+      frappe.throw('Number of vehicles for distribution can be a maximum 3 digit number')
+  }
+    
     
   }
 
